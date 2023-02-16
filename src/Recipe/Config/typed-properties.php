b652be813d715be00a91696365a562edb8d23266
Jan Mikeš <j.mikes@me.com>
2023-02-17 00:07:40 +0100

    Fix tests

--- src/Recipe/Config/typed-properties.php
+++ src/Recipe/Config/typed-properties.php
@@ -1,8 +1,15 @@
 <?php
 
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictGetterMethodReturnTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(TypedPropertyRector::class);
+    $rectorConfig->rule(TypedPropertyFromStrictConstructorRector::class);
+    $rectorConfig->rule(TypedPropertyFromStrictGetterMethodReturnTypeRector::class);
+
+    $rectorConfig->ruleWithConfiguration(TypedPropertyFromAssignsRector::class, [
+        TypedPropertyFromAssignsRector::INLINE_PUBLIC => false,
+    ]);
 };
